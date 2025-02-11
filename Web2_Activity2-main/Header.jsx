34e0cc5b9e248d30740832699cb28a33aa9a6ff5
juneangel12 <juneangelmontecastro12@gmail.com>
2025-02-11 13:22:17 +0800

    Update Header.jsx

--- Web2_Activity2-main/Header.jsx
+++ Web2_Activity2-main/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = (props) => {
   return (
     <div className='header flex flex-col items-center justify-center'>
-      <img src="/src/assets/images/solo.jpg" alt="" className='w-30 h-30 round rounded-full'/>
+      <img src="/src/assets/images/june.jpg" alt="" className='w-30 h-30 round rounded-full'/>
      <h2 className='text-1xl font-bold'>{props.name}</h2>
      <p className='text w-80 text-xs'>A web developer with a passion for building user-friendly and accessible applications.</p>
     </div>
